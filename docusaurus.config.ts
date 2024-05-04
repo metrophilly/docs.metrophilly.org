20b582f2f3dadaa9a1d29de5c36ffdbd09e4e4b2
Paul H Choi <paulhchoi+github@pm.me>
2024-05-04 14:18:39 -0400

    fix deployment branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "metrophilly", // Usually your GitHub org/user name.
   projectName: "docs.metrophilly.org", // Usually your repo name.
+  deploymentBranch: "main",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
